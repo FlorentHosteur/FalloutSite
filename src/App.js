448913e3469ba5140f84bfdea187c62ef676c422
Florent Gentric <florent@hosteur.com>
2023-04-01 10:15:28 +0200

    images

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <img src="/path/to/logo.png" alt="Logo de groupe" className="logo" />
+        <img src="https://fallout.s3.hosteur.io/sites/Fallout-final-logo-2014-inv.png" alt="Fallout" className="logo" />
         <nav>
           <ul>
             <li>
-              <a href="https://www.facebook.com/">Facebook</a>
+              <a href="https://www.facebook.com/fallout.metal.band">Facebook</a>
             </li>
             <li>
               <a href="https://www.instagram.com/">Instagram</a>
@@ -28,23 +28,23 @@ function App() {
       </section>
       <section className="two-columns">
         <div className="column">
-          <img src="/path/to/image1.jpg" alt="Image 1" />
+          <img src="https://fallout.s3.hosteur.io/sites/Photo-gallery-6.jpg" alt="Image 1" />
           <p>Texte explicatif pour l'image 1</p>
         </div>
         <div className="column">
-          <img src="/path/to/image2.jpg" alt="Image 2" />
+          <img src="https://fallout.s3.hosteur.io/sites/singer3.jpg" alt="Image 2" />
           <p>Texte explicatif pour l'image 2</p>
         </div>
       </section>
       <section className="three-rows">
         <div className="row">
-          <img src="/path/to/image3.jpg" alt="Image 3" />
+          <img src="https://fallout.s3.hosteur.io/sites/Photo-gallery-1.jpg" alt="Image 3" />
         </div>
         <div className="row">
-          <img src="/path/to/image4.jpg" alt="Image 4" />
+          <img src="https://fallout.s3.hosteur.io/sites/Photo-gallery-3.jpg" alt="Image 4" />
         </div>
         <div className="row">
-          <img src="/path/to/image5.jpg" alt="Image 5" />
+          <img src="https://fallout.s3.hosteur.io/sites/Photo-gallery-2.jpg" alt="Image 5" />
         </div>
       </section>
       <footer className="footer">
